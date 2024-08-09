c38b8294e07598c6881f778bad28440ee4d7a006
James Foucar <jgfouca@sandia.gov>
2024-08-09 12:54:30 -0600

    timed_p4

--- mdrange.cxx
+++ mdrange.cxx
@@ -39,6 +39,57 @@ void print_iteration(T policy, const std::string& name, int dim1, int dim2, int 
   });
 }
 
+KOKKOS_INLINE_FUNCTION
+void unflatten_idx(const int idx, const Kokkos::Array<int, 2>& dims, int& i, int& j)
+{
+  i = idx / dims[1];
+  j = idx % dims[1];
+}
+
+KOKKOS_INLINE_FUNCTION
+void unflatten_idx(const int idx, const Kokkos::Array<int, 3>& dims, int& i, int& j, int& k)
+{
+  i = (idx / dims[2]) / dims[1];
+  j = (idx / dims[2]) % dims[1];
+  k =  idx % dims[2];
+}
+
+KOKKOS_INLINE_FUNCTION
+void unflatten_idx_rev(const int idx, const Kokkos::Array<int, 3>& dims, int& i, int& j, int& k)
+{
+  // i = (idx / dims[2]) / dims[1];
+  // j = (idx / dims[2]) % dims[1];
+  // k =  idx % dims[2];
+  i = idx % dims[0];
+  j = (idx / dims[0]) % dims[1];
+  k = (idx / dims[0]) / dims[1];
+}
+
+void print_iteration_flat(int dim1, int dim2, int dim3)
+{
+  const int tot = dim1*dim2*dim3;
+  Kokkos::Array<int, 3> dims = {dim1, dim2, dim3};
+  std::cout << "Iterating flat" << std::endl;
+  Kokkos::parallel_for(tot, KOKKOS_LAMBDA(int idx) {
+    int i, j, k;
+    unflatten_idx(idx, dims, i, j, k);
+    cout << "  Iterating (" << i << ", " << j << ", " << k << ")" << endl;
+  });
+}
+
+void print_iteration_flat_rev(int dim1, int dim2, int dim3)
+{
+  const int tot = dim1*dim2*dim3;
+  Kokkos::Array<int, 3> dims = {dim1, dim2, dim3};
+  std::cout << "Iterating flat" << std::endl;
+  Kokkos::parallel_for(tot, KOKKOS_LAMBDA(int idx) {
+    int i, j, k;
+    unflatten_idx_rev(idx, dims, i, j, k);
+    cout << "  Iterating (" << i << ", " << j << ", " << k << ")" << endl;
+  });
+}
+
+
 int main(int argc, char** argv)
 {
   Kokkos::initialize(argc, argv); {
@@ -57,6 +108,9 @@ int main(int argc, char** argv)
     print_iteration(MDRangeP3L <>({0,0,0}, {dim1, dim2, dim3}, {1,1,1}), "MDRangeP3L_tile",  2, 3, 4);
     print_iteration(MDRangeP3R <>({0,0,0}, {dim1, dim2, dim3}, {1,1,1}), "MDRangeP3R_tile",  2, 3, 4);
     print_iteration(MDRangeP3D <>({0,0,0}, {dim1, dim2, dim3}, {1,1,1}), "MDRangeP3D_tile",  2, 3, 4);
+
+    print_iteration_flat(2, 3, 4);
+    print_iteration_flat_rev(2, 3, 4);
 
     std::cout << "Validating" << std::endl;
     for (int k = 0; k < dim3; ++k) {
---
+++ timed_p4.cxx
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <Kokkos_Core.hpp>
+
+using namespace std;
+
+#define TIMED_P4(name, kernel)                  \
+  {                                             \
+  auto start_t = std::chrono::high_resolution_clock::now();     \
+  kernel;                                                               \
+  auto stop_t = std::chrono::high_resolution_clock::now();              \
+  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop_t - start_t); \
+  static double total_s = 0.; \
+  total_s += duration.count() / 1000000.0; \
+  std::cout << name << " total is: " << total_s << " s" << std::endl;   \
+  }
+
+int main(int argc, char** argv)
+{
+  Kokkos::initialize(argc, argv); {
+
+    Kokkos::View<int*> view("view", 4);
+    Kokkos::deep_copy(view, 4);
+
+    for (int j = 0; j < 4; ++j) {
+      TIMED_P4("Name", Kokkos::parallel_for(4, KOKKOS_LAMBDA(int i) {
+            cout << view(i) << endl;
+          }));
+    }
+  }
+  Kokkos::finalize();
+  return 0;
+}
