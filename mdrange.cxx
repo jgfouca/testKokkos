a63fdae88928633a19b9630e39eefadf89156aa4
James Foucar <jgfouca@sandia.gov>
2024-12-19 16:54:32 -0700

    Better asserting

--- mdrange.cxx
+++ mdrange.cxx
@@ -49,9 +49,17 @@ void unflatten_idx(const int idx, const Kokkos::Array<int, 2>& dims, int& i, int
 KOKKOS_INLINE_FUNCTION
 void unflatten_idx(const int idx, const Kokkos::Array<int, 3>& dims, int& i, int& j, int& k)
 {
-  i = (idx / dims[2]) / dims[1];
+  i = idx / (dims[2] * dims[1]);
   j = (idx / dims[2]) % dims[1];
   k =  idx % dims[2];
+}
+
+KOKKOS_INLINE_FUNCTION
+void unflatten_idx_rev(const int idx, const Kokkos::Array<int, 3>& dims, int& i, int& j, int& k)
+{
+  i = idx % dims[0];
+  j = (idx / dims[0]) % dims[1];
+  k = idx / (dims[0] * dims[1]);
 }
 
 KOKKOS_INLINE_FUNCTION
@@ -63,52 +71,14 @@ void unflatten_idx(const int idx, const Kokkos::Array<int, 4>& dims, int& i, int
   l = idx % dims[3];
 }
 
-
 KOKKOS_INLINE_FUNCTION
-void unflatten_idx_rev(const int idx, const Kokkos::Array<int, 3>& dims, int& i, int& j, int& k)
+void unflatten_idx_rev(const int idx, const Kokkos::Array<int, 4>& dims, int& i, int& j, int& k, int& l)
 {
   i = idx % dims[0];
   j = (idx / dims[0]) % dims[1];
-  k = (idx / dims[0]) / dims[1];
+  k = (idx / (dims[0]*dims[1])) % dims[2];
+  l = idx / (dims[0]*dims[1]*dims[2]);
 }
-
-void print_iteration_flat(int dim1, int dim2, int dim3)
-{
-  const int tot = dim1*dim2*dim3;
-  Kokkos::Array<int, 3> dims = {dim1, dim2, dim3};
-  std::cout << "Iterating flat" << std::endl;
-  Kokkos::parallel_for(tot, KOKKOS_LAMBDA(int idx) {
-    int i, j, k;
-    unflatten_idx(idx, dims, i, j, k);
-    cout << "  Iterating (" << i << ", " << j << ", " << k << ")" << endl;
-  });
-}
-
-void print_iteration_flat_rev(int dim1, int dim2, int dim3)
-{
-  const int tot = dim1*dim2*dim3;
-  Kokkos::Array<int, 3> dims = {dim1, dim2, dim3};
-  std::cout << "Iterating flat" << std::endl;
-  Kokkos::parallel_for(tot, KOKKOS_LAMBDA(int idx) {
-    int i, j, k;
-    unflatten_idx_rev(idx, dims, i, j, k);
-    cout << "  Iterating (" << i << ", " << j << ", " << k << ")" << endl;
-  });
-}
-
-
-void print_iteration_flat4(int dim1, int dim2, int dim3, int dim4)
-{
-  const int tot = dim1*dim2*dim3*dim4;
-  Kokkos::Array<int, 4> dims = {dim1, dim2, dim3, dim4};
-  std::cout << "Iterating flat" << std::endl;
-  Kokkos::parallel_for(tot, KOKKOS_LAMBDA(int idx) {
-    int i, j, k, l;
-    unflatten_idx(idx, dims, i, j, k, l);
-    cout << "  Iterating (" << i << ", " << j << ", " << k << ", " << l << ")" << endl;
-  });
-}
-
 
 int main(int argc, char** argv)
 {
@@ -116,38 +86,92 @@ int main(int argc, char** argv)
     int dim1 = 2;
     int dim2 = 3;
     int dim3 = 4;
+    int dim4 = 5;
 
-    print_iteration(MDRangeP3RL<>({0,0,0}, {dim1, dim2, dim3}), "MDRangeP3RL", 2, 3, 4);
-    print_iteration(MDRangeP3LR<>({0,0,0}, {dim1, dim2, dim3}), "MDRangeP3LR", 2, 3, 4);
-    print_iteration(MDRangeP3L <>({0,0,0}, {dim1, dim2, dim3}), "MDRangeP3L",  2, 3, 4);
-    print_iteration(MDRangeP3R <>({0,0,0}, {dim1, dim2, dim3}), "MDRangeP3R",  2, 3, 4);
-    print_iteration(MDRangeP3D <>({0,0,0}, {dim1, dim2, dim3}), "MDRangeP3D",  2, 3, 4);
+    print_iteration(MDRangeP3RL<>({0,0,0}, {dim1, dim2, dim3}), "MDRangeP3RL", dim1, dim2, dim3);
+    print_iteration(MDRangeP3LR<>({0,0,0}, {dim1, dim2, dim3}), "MDRangeP3LR", dim1, dim2, dim3);
+    print_iteration(MDRangeP3L <>({0,0,0}, {dim1, dim2, dim3}), "MDRangeP3L",  dim1, dim2, dim3);
+    print_iteration(MDRangeP3R <>({0,0,0}, {dim1, dim2, dim3}), "MDRangeP3R",  dim1, dim2, dim3);
+    print_iteration(MDRangeP3D <>({0,0,0}, {dim1, dim2, dim3}), "MDRangeP3D",  dim1, dim2, dim3);
 
-    print_iteration(MDRangeP3RL<>({0,0,0}, {dim1, dim2, dim3}, {1,1,1}), "MDRangeP3RL_tile", 2, 3, 4);
-    print_iteration(MDRangeP3LR<>({0,0,0}, {dim1, dim2, dim3}, {1,1,1}), "MDRangeP3LR_tile", 2, 3, 4);
-    print_iteration(MDRangeP3L <>({0,0,0}, {dim1, dim2, dim3}, {1,1,1}), "MDRangeP3L_tile",  2, 3, 4);
-    print_iteration(MDRangeP3R <>({0,0,0}, {dim1, dim2, dim3}, {1,1,1}), "MDRangeP3R_tile",  2, 3, 4);
-    print_iteration(MDRangeP3D <>({0,0,0}, {dim1, dim2, dim3}, {1,1,1}), "MDRangeP3D_tile",  2, 3, 4);
+    // print_iteration(MDRangeP3RL<>({0,0,0}, {dim1, dim2, dim3}, {1,1,1}), "MDRangeP3RL_tile", dim1, dim2, dim3);
+    // print_iteration(MDRangeP3LR<>({0,0,0}, {dim1, dim2, dim3}, {1,1,1}), "MDRangeP3LR_tile", dim1, dim2, dim3);
+    // print_iteration(MDRangeP3L <>({0,0,0}, {dim1, dim2, dim3}, {1,1,1}), "MDRangeP3L_tile",  dim1, dim2, dim3);
+    // print_iteration(MDRangeP3R <>({0,0,0}, {dim1, dim2, dim3}, {1,1,1}), "MDRangeP3R_tile",  dim1, dim2, dim3);
+    // print_iteration(MDRangeP3D <>({0,0,0}, {dim1, dim2, dim3}, {1,1,1}), "MDRangeP3D_tile",  dim1, dim2, dim3);
 
-    print_iteration_flat(2, 3, 4);
-    print_iteration_flat_rev(2, 3, 4);
+    std::cout << "YAKL fortran" << std::endl;
+    parallel_for( YAKL_AUTO_LABEL() , yakl::fortran::SimpleBounds<3>(dim1, dim2, dim3) , YAKL_LAMBDA (int i, int j, int k) {
+      cout << "  Iterating (" << i-1 << ", " << j-1 << ", " << k-1 << ")" << endl;
+    });
 
-    std::cout << "Validating" << std::endl;
-    for (int k = 0; k < dim3; ++k) {
+    std::cout << "YAKL c" << std::endl;
+    parallel_for( YAKL_AUTO_LABEL() , yakl::c::SimpleBounds<3>(dim1, dim2, dim3) , YAKL_LAMBDA (int i, int j, int k) {
+      cout << "  Iterating (" << i << ", " << j << ", " << k << ")" << endl;
+    });
+
+    std::cout << "Validating unflattens" << std::endl;
+    Kokkos::Array<int, 3> dims3 = {dim1, dim2, dim3};
+    int idx = 0;
+    for (int i = 0; i < dim1; ++i) {
       for (int j = 0; j < dim2; ++j) {
-        for (int i = 0; i < dim1; ++i) {
-          cout << "  Iterating (" << i << ", " << j << ", " << k << ")" << endl;
+        for (int k = 0; k < dim3; ++k, ++idx) {
+          int iv, jv, kv;
+          unflatten_idx(idx, dims3, iv, jv, kv);
+          assert(k == kv);
+          assert(j == jv);
+          assert(i == iv);
         }
       }
     }
 
-    std::cout << "YAKL" << std::endl;
-    using yakl::fortran::SimpleBounds;
-    parallel_for( YAKL_AUTO_LABEL() , SimpleBounds<3>(dim1, dim2, dim3) , YAKL_LAMBDA (int i, int j, int k) {
-      cout << "  Iterating (" << i-1 << ", " << j-1 << ", " << k-1 << ")" << endl;
-    });
+    idx = 0;
+    for (int k = 0; k < dim3; ++k) {
+      for (int j = 0; j < dim2; ++j) {
+        for (int i = 0; i < dim1; ++i, ++idx) {
+          int iv, jv, kv;
+          unflatten_idx_rev(idx, dims3, iv, jv, kv);
+          assert(k == kv);
+          assert(j == jv);
+          assert(i == iv);
+        }
+      }
+    }
 
-    print_iteration_flat4(2, 3, 4, 5);
+    Kokkos::Array<int, 4> dims4 = {dim1, dim2, dim3, dim4};
+
+    idx = 0;
+    for (int i = 0; i < dim1; ++i) {
+      for (int j = 0; j < dim2; ++j) {
+        for (int k = 0; k < dim3; ++k) {
+          for (int l = 0; l < dim4; ++l, ++idx) {
+            int iv, jv, kv, lv;
+            unflatten_idx(idx, dims4, iv, jv, kv, lv);
+            assert(l == lv);
+            assert(k == kv);
+            assert(j == jv);
+            assert(i == iv);
+          }
+        }
+      }
+    }
+
+    idx = 0;
+    for (int l = 0; l < dim4; ++l) {
+      for (int k = 0; k < dim3; ++k) {
+        for (int j = 0; j < dim2; ++j) {
+          for (int i = 0; i < dim1; ++i, ++idx) {
+            int iv, jv, kv, lv;
+            unflatten_idx_rev(idx, dims4, iv, jv, kv, lv);
+            assert(l == lv);
+            assert(k == kv);
+            assert(j == jv);
+            assert(i == iv);
+          }
+        }
+      }
+    }
+
   }
   Kokkos::finalize();
   return 0;
